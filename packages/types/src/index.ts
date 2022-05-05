16a041d68e5d8e342e953283e253e6210971f864
Akram Hussein <akram@vektor.finance>
2022-05-05 19:13:36 -0300

    fix: replace the signing tx payload types with the ones defined in the signing common lib

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,6 +1,5 @@
 export * from '@vektor-finance/api' // ideally, these types should be here and imported into @vektor-finance/api from here
 export * from './gateway'
 export * from './signing'
-export * from './transaction_payload'
 export * from './task'
 export * from './vxl'
