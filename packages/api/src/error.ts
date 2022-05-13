db03b4699e2dbbe8c9a19f6e750ba2ad795e5dc6
Akram Hussein <akram@vektor.finance>
2022-05-13 17:02:13 -0300

    fix(api): update types for compiler errors (#19)
    * fix(api): update types for compiler errors
    
    * fix: refactor errors

--- packages/api/src/error.ts
+++ packages/api/src/error.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosError } from 'axios'
 
+import { SubfunctionSpec, VDN, VDNType } from './models'
+
 // API Errors
 export const isAPIError = (payload: any): payload is AxiosError<any> => axios.isAxiosError(payload)
 
@@ -11,73 +13,74 @@ export interface TokenInfo {
   offset: number
 }
 
-export interface FunctionArg {
-  type: string
-  value: string
+export interface CompilerBaseError {
+  token_info: TokenInfo
 }
 
-export interface FunctionData {
+export interface CompilerIncorrectArgumentCountError extends CompilerBaseError {
   function: string
   subfunction: string
   argument_counts: number[]
-  args: (FunctionArg | null)[]
-  opts: Record<string, FunctionArg>
+  args: (VDN | null)[]
+  options: Record<string, VDN | null>
+}
+
+export interface CompilerUndefinedFunctionError {
+  token_info: TokenInfo
+  name: string
+  argument_count: number
+}
+
+export interface IncorrectlyTyped {
+  name: string
+  expected_type: VDNType
+  provided_type: VDNType
+}
+
+export interface SignatureMismatch {
+  incorrectly_typed_args: IncorrectlyTyped[]
+  incorrectly_typed_options: IncorrectlyTyped[]
+  unspecified_options: string[]
+}
+
+export interface CompilerSignatureMismatchError extends CompilerBaseError {
+  function: string
+  subfunction: string
+  specs_and_mismatches: {
+    mismatch: SignatureMismatch[]
+    spec: SubfunctionSpec
+  }[]
+}
+
+export interface CompilerAmbiguousCallError extends CompilerBaseError {
+  function: string
+  subfunction: string
 }
 
 export type VXLSubmitAPIError =
   | {
-      type: 'runtime_error'
+      type: 'runtime_error' | 'parser_error' | 'unknown_error'
       data: Record<string, unknown>
+    }
+  | {
+      type: 'compiler_options_before_parameters_error' | 'compiler_undefined_symbol_error'
+      data: CompilerBaseError
     }
   | {
       type: 'compiler_undefined_function_error'
-      data: {
-        token_info: TokenInfo
-        name: string
-        argument_count: number
-      }
+      data: CompilerUndefinedFunctionError
     }
   | {
       type: 'compiler_incorrect_argument_count_error'
-      data: {
-        token_info: TokenInfo
-      } & FunctionData
-    }
-  | {
-      type: 'compiler_options_before_parameters_error'
-      data: {
-        token_info: TokenInfo
-      }
-    }
-  | {
-      type: 'compiler_undefined_symbol_error'
-      data: {
-        token_info: TokenInfo
-      }
+      data: CompilerIncorrectArgumentCountError
     }
   | {
       type: 'compiler_ambiguous_call_error'
-      data: {
-        token_info: TokenInfo
-        function: string
-        subfunction: string
-      }
+      data: CompilerAmbiguousCallError
     }
   | {
-      type: 'compiler_arg_types_dont_match_signatures_error'
-      data: {
-        token_info: TokenInfo
-        function: string
-        subfunction: string
-      }
-    }
-  | {
-      type: 'parser_error'
-      data: Record<string, unknown>
-    }
-  | {
-      type: 'unknown_error'
-      data: Record<string, unknown>
+      type: 'compiler_signature_mismatch_error'
+      data: CompilerSignatureMismatchError
     }
 
 export type VXLSubmitAPIErrorType = VXLSubmitAPIError['type']
