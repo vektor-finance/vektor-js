9d8925b8b737f401bd4e701c41df441b982795f7
Akram Hussein <akram@vektor.finance>
2022-05-13 16:44:05 -0300

    fix: refactor errors

--- packages/api/src/error.ts
+++ packages/api/src/error.ts
@@ -13,12 +13,22 @@ export interface TokenInfo {
   offset: number
 }
 
-export interface FunctionData {
+export interface CompilerBaseError {
+  token_info: TokenInfo
+}
+
+export interface CompilerIncorrectArgumentCountError extends CompilerBaseError {
   function: string
   subfunction: string
   argument_counts: number[]
   args: (VDN | null)[]
-  opts: Record<string, VDN | null>
+  options: Record<string, VDN | null>
+}
+
+export interface CompilerUndefinedFunctionError {
+  token_info: TokenInfo
+  name: string
+  argument_count: number
 }
 
 export interface IncorrectlyTyped {
@@ -33,64 +43,44 @@ export interface SignatureMismatch {
   unspecified_options: string[]
 }
 
+export interface CompilerSignatureMismatchError extends CompilerBaseError {
+  function: string
+  subfunction: string
+  specs_and_mismatches: {
+    mismatch: SignatureMismatch[]
+    spec: SubfunctionSpec
+  }[]
+}
+
+export interface CompilerAmbiguousCallError extends CompilerBaseError {
+  function: string
+  subfunction: string
+}
+
 export type VXLSubmitAPIError =
   | {
-      type: 'runtime_error'
+      type: 'runtime_error' | 'parser_error' | 'unknown_error'
       data: Record<string, unknown>
+    }
+  | {
+      type: 'compiler_options_before_parameters_error' | 'compiler_undefined_symbol_error'
+      data: CompilerBaseError
     }
   | {
       type: 'compiler_undefined_function_error'
-      data: {
-        token_info: TokenInfo
-        name: string
-        argument_count: number
-      }
+      data: CompilerUndefinedFunctionError
     }
   | {
       type: 'compiler_incorrect_argument_count_error'
-      data: {
-        token_info: TokenInfo
-      } & FunctionData
-    }
-  | {
-      type: 'compiler_options_before_parameters_error'
-      data: {
-        token_info: TokenInfo
-      }
-    }
-  | {
-      type: 'compiler_undefined_symbol_error'
-      data: {
-        token_info: TokenInfo
-      }
+      data: CompilerIncorrectArgumentCountError
     }
   | {
       type: 'compiler_ambiguous_call_error'
-      data: {
-        token_info: TokenInfo
-        function: string
-        subfunction: string
-      }
+      data: CompilerAmbiguousCallError
     }
   | {
       type: 'compiler_signature_mismatch_error'
-      data: {
-        token_info: TokenInfo
-        function: string
-        subfunction: string
-        specs_and_mismatches: {
-          mismatch: SignatureMismatch[]
-          spec: SubfunctionSpec
-        }[]
-      }
-    }
-  | {
-      type: 'parser_error'
-      data: Record<string, unknown>
-    }
-  | {
-      type: 'unknown_error'
-      data: Record<string, unknown>
+      data: CompilerSignatureMismatchError
     }
 
 export type VXLSubmitAPIErrorType = VXLSubmitAPIError['type']
