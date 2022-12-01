7266b28f6a70909599092b4c0fc8457a3fc862f3
Akram Hussein <akram@vektor.finance>
2022-12-01 12:53:07 -0300

    fix: replace RuntimeError with one defined outside of OpenAPI

--- packages/api/src/error.ts
+++ packages/api/src/error.ts
@@ -150,6 +150,11 @@ export type CompilerError =
 
 // Runtime Errors
 
+export interface RuntimeError {
+  type: 'runtime_error'
+  data: Record<string, unknown>
+}
+
 export interface RuntimeSignatureMismatchError {
   type: 'runtime_signature_mismatch_error'
   data: { function: string; subfunction: string } & SpecAndMismatches
@@ -157,7 +162,7 @@ export interface RuntimeSignatureMismatchError {
 
 export type VXLSubmitAPIError =
   |
-  CompilerError | RuntimeSignatureMismatchError | {
+  CompilerError | RuntimeError | RuntimeSignatureMismatchError | {
     type: 'runtime_error' | 'parser_error' | 'unknown_error' | 'local_compiler_mixed_functions_error'
     data: Record<string, unknown>
   }
