bf04f3a4ef0f47e41690bd95d4552b3ffaf9d2d9
Akram Hussein <akram@vektor.finance>
2022-12-01 10:17:06 -0300

    chore(types): add RuntimeSignatureMismatchError

--- packages/api/src/error.ts
+++ packages/api/src/error.ts
@@ -148,11 +148,23 @@ export type CompilerError =
       data: CompilerEmptyListLiteralError
     }
 
+// Runtime Errors
+
+export interface RuntimeError {
+  type: 'runtime_error'
+  data: Record<string, unknown>
+}
+
+export interface RuntimeSignatureMismatchError {
+  type: 'runtime_signature_mismatch_error'
+  data: { function: string; subfunction: string } & SpecAndMismatches
+}
+
 export type VXLSubmitAPIError =
-  | {
-      type: 'runtime_error' | 'parser_error' | 'unknown_error' | 'local_compiler_mixed_functions_error'
-      data: Record<string, unknown>
-    }
-  | CompilerError
+  |
+  CompilerError | RuntimeError | RuntimeSignatureMismatchError | {
+    type: 'parser_error' | 'unknown_error' | 'local_compiler_mixed_functions_error'
+    data: Record<string, unknown>
+  }
 
 export type VXLSubmitAPIErrorType = VXLSubmitAPIError['type']
