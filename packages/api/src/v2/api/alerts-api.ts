dc16420e02efa158dc7226d90da1a6b6106467cb
Gabriel Moraes <gabrielhm2001@gmail.com>
2023-06-22 10:13:49 -0300

    feat(api): add new alerts api

---
+++ packages/api/src/v2/api/alerts-api.ts
@@ -0,0 +1,39 @@
+import { Alert } from '../../models/alert'
+import { BaseApi } from '../base'
+
+type AlertsListParams = {
+  triggered?: boolean
+}
+
+/**
+ *
+ */
+export class AlertsApi extends BaseApi {
+  /**
+   *
+   */
+  public delete(alertId: string) {
+    return this.httpClient.delete<void>(`/alerts/${alertId}`)
+  }
+
+  /**
+   *
+   */
+  public deleteAll() {
+    return this.httpClient.delete<void>(`/alerts`)
+  }
+
+  /**
+   *
+   */
+  public get(alertId: string) {
+    return this.httpClient.get<Alert>(`/alerts/${alertId}`)
+  }
+
+  /**
+   *
+   */
+  public list(params?: AlertsListParams) {
+    return this.httpClient.get<Alert[]>(`/alerts`, { params })
+  }
+}
