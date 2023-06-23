453f561075297a1c4974f8b285e92f52556e660e
Gabriel Moraes <gabrielhm2001@gmail.com>
2023-06-23 10:10:56 -0300

    chore(types): remove unused type

--- packages/types/src/api/resource-common.ts
+++ packages/types/src/api/resource-common.ts
@@ -2,20 +2,6 @@ import { ID } from './id'
 import { Timestamps } from './timestamps'
 
 /**
- *
- * @export
- * @interface ResourceCommonAllOf
- */
-export interface ResourceCommonAllOf {
-  /**
-   *
-   * @type {ID}
-   * @memberof ResourceCommonAllOf
-   */
-  id: ID
-}
-
-/**
  * @type ResourceCommon
  * Resource common values
  * @export
