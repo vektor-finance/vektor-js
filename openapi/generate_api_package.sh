39b1b5c138eb7d533a0be8f27d26166043fa58d3
Gabriel Moraes <gabrielhm2001@gmail.com>
2023-06-23 14:22:46 -0300

    chore: delete openapi files

--- openapi/generate_api_package.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euf -o pipefail
-
-DIR="$(dirname "$(which "$0")")"
-
-export TS_POST_PROCESS_FILE="node_modules/.bin/prettier --write"
-openapi-generator generate -c ${DIR}/configuration.yaml -t ${DIR}
