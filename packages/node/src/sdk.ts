41179a3fa26c7c6b7db76132223a4bfd7abd9b91
Gabriel Moraes <gabrielhm2001@gmail.com>
2023-06-23 14:58:58 -0300

    refactor(node): update to use new api version

--- packages/node/src/sdk.ts
+++ packages/node/src/sdk.ts
@@ -1,4 +1,4 @@
-import { API, APIOptions, options as apiOptions } from '@vektor-finance/api'
+import { Api, ApiConfig, defaultApiConfig } from '@vektor-finance/api'
 import { Gateway, GatewayOptions, options as gatewayOptions } from '@vektor-finance/gateway'
 import { Socket } from 'phoenix-channels'
 
@@ -8,7 +8,7 @@ import { Socket } from 'phoenix-channels'
  */
 export interface VektorOptions {
   /** API client options */
-  api?: APIOptions
+  api?: ApiConfig
 
   /** Gateway client options */
   gateway?: GatewayOptions
@@ -18,11 +18,11 @@ export type VektorOptionsType = 'default' | 'local'
 
 export const options: Record<VektorOptionsType, VektorOptions> = {
   default: {
-    api: apiOptions.default,
+    api: defaultApiConfig,
     gateway: gatewayOptions.default,
   },
   local: {
-    api: apiOptions.local,
+    api: {},
     gateway: gatewayOptions.local,
   },
 }
@@ -39,7 +39,7 @@ export const options: Record<VektorOptionsType, VektorOptions> = {
  * ```
  */
 export class Vektor {
-  public readonly api: API
+  public readonly api: Api
   public readonly gateway: Gateway
 
   /**
@@ -49,7 +49,7 @@ export class Vektor {
    * @param options Options to configure the SDK and sub-clients.
    */
   public constructor(authToken: string, options?: VektorOptions) {
-    this.api = new API(options?.api)
+    this.api = new Api(options?.api)
     this.gateway = new Gateway(Socket, authToken, options?.gateway)
   }
 }
