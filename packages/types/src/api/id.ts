5e9531bce4c10f8cd2a98d0747bf65bbd399e5de
Gabriel Moraes <gabrielhm2001@gmail.com>
2023-06-26 12:05:13 -0300

    refactor(types): simplify label types

--- packages/types/src/api/id.ts
+++
@@ -1,8 +0,0 @@
-export interface ID {
-  /**
-   * Universally unique identifier - UUID V4
-   * @type {string}
-   * @memberof ID
-   */
-  id: string
-}
--- packages/types/src/api/label.ts
+++ packages/types/src/api/label.ts
@@ -1,107 +1,17 @@
-import { ID } from './id'
 import { NetworkMode } from './network'
-import { Timestamps } from './timestamps'
 
-/**
- * Common properties of a Label schema
- * @export
- * @interface LabelCommon
- */
-export interface LabelCommon {
-  /**
-   * Unique name to identify the Label
-   * @type {string}
-   * @memberof LabelCommon
-   */
-  name: string
-  /**
-   * Network
-   * @type {string}
-   * @memberof LabelCommon
-   */
-  network: string
-  /**
-   * Label\'s address
-   * @type {string}
-   * @memberof LabelCommon
-   */
-  address?: string
-}
-
-/**
- * @type LabelUpdateRequest
- * Request to update a label
- * @export
- */
-export type LabelUpdateRequest = LabelCommonOptional
-
-/**
- * @type LabelCreateRequest
- * Request to create a label
- * @export
- */
-export type LabelCreateRequest = LabelCommonRequired
-
-/**
- * @type Label
- * Identifier to easily reference an Address or Transaction
- * @export
- */
-export type Label = LabelCommonRequired & ID & Timestamps
-
-/**
- * @type LabelResponse
- * Response for a single label
- * @export
- */
-export type LabelResponse = Label
-
-/**
- * Common properties of a Label (optional)
- * @export
- * @interface LabelCommonOptional
- */
-export interface LabelCommonOptional {
-  /**
-   * Label\'s address
-   * @type {string}
-   * @memberof LabelCommonOptional
-   */
-  address?: string
-  /**
-   * Unique name to identify the Label
-   * @type {string}
-   * @memberof LabelCommonOptional
-   */
-  name?: string
-  /**
-   *
-   * @type {NetworkMode}
-   * @memberof LabelCommonOptional
-   */
-  network_mode?: NetworkMode
-}
-
-export interface LabelCommonRequired {
-  /**
-   * Label\'s address
-   * @type {string}
-   * @memberof LabelCommonRequired
-   */
+export interface Label {
   address: string
-  /**
-   * Unique name to identify the Label
-   * @type {string}
-   * @memberof LabelCommonRequired
-   */
   name: string
-  /**
-   *
-   * @type {NetworkMode}
-   * @memberof LabelCommonRequired
-   */
   network_mode: NetworkMode
+  id: string
+  created_at: string
+  updated_at: string
 }
+
+export type LabelUpdateRequest = Partial<Pick<Label, 'address' | 'name' | 'network_mode'>>
+
+export type LabelCreateRequest = Pick<Label, 'address' | 'name' | 'network_mode'>
 
 export interface LabelsListRequestParams {
   networkMode?: NetworkMode
