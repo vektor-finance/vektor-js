6b91f0c8961c2a3e2195677126a06622a8ac4a45
Akram Hussein <akram@vektor.finance>
2023-07-04 12:52:49 -0300

    feat(types): add types for user settings

---
+++ packages/types/src/api/user.ts
@@ -0,0 +1,66 @@
+/**
+ * User display settings
+ * @export
+ * @interface UserDisplaySettings
+ */
+export interface UserDisplaySettings {
+    /**
+     * Number of decimals to show
+     * @type {number}
+     * @memberof UserDisplaySettings
+     */
+    decimals: number
+    /**
+     * If TRUE, icons for assets, venues, blockchains etc will be shown
+     * @type {boolean}
+     * @memberof UserDisplaySettings
+     */
+    show_icons: boolean
+    /**
+     * If TRUE, data is formatted
+     * @type {boolean}
+     * @memberof UserDisplaySettings
+     */
+    show_formatted_data: boolean
+    /**
+     * If TRUE, sensitive fields will not be obfuscated e.g. balances
+     * @type {boolean}
+     * @memberof UserDisplaySettings
+     */
+    show_sensitive_fields: boolean
+}
+
+/**
+ * User autocomplete settings
+ * @export
+ * @interface UserAutocompleteSettings
+ */
+export interface UserAutocompleteSettings {
+    /**
+     * If TRUE, pressing ENTER will accept an autocomplete suggestion. Otherwise, TAB is used
+     * @type {boolean}
+     * @memberof UserAutocompleteSettings
+     */
+    enter_accepts_suggestions: boolean
+}
+
+/**
+ * User settings
+ * @export
+ * @interface UserSettings
+ */
+export interface UserSettings {
+    /**
+     * User's display settings
+     * @type {UserDisplaySettings}
+     * @memberof UserSettings
+     */
+    display: UserDisplaySettings
+
+    /**
+     * User's autocomplete settings
+     * @type {UserAutocompleteSettings}
+     * @memberof UserSettings
+     */
+    autocomplete: UserAutocompleteSettings
+  }
---
+++ packages/types/src/api/vdn-user-settings.ts
@@ -0,0 +1,61 @@
+import { VDNBase } from './vdn-base'
+import { VDNBoolean } from './vdn-boolean'
+import { VDNInteger } from './vdn-integer'
+
+/**
+ *
+ * @export
+ * @interface VDNUserSettingsValue
+ */
+export interface VDNUserSettingsValue {
+  /**
+   *
+   * @type {VDNInteger}
+   * @memberof VDNUserSettingsValue
+   */
+  decimals: VDNInteger
+  /**
+   *
+   * @type {VDNBoolean}
+   * @memberof VDNUserSettingsValue
+   */
+  show_icons: VDNBoolean
+  /**
+   *
+   * @type {VDNBoolean}
+   * @memberof VDNUserSettingsValue
+   */
+  show_formatted_data: VDNBoolean
+  /**
+   *
+   * @type {VDNBoolean}
+   * @memberof VDNUserSettingsValue
+   */
+  show_sensitive_fields: VDNBoolean
+  /**
+   *
+   * @type {VDNBoolean}
+   * @memberof VDNUserSettingsValue
+   */
+  enter_accepts_suggestions: VDNBoolean
+}
+
+/**
+ * VXL user_settings representation
+ * @export
+ * @interface VDNUserSettings
+ */
+export interface VDNUserSettings extends VDNBase {
+  /**
+   *
+   * @type {string}
+   * @memberof VDNUserSettings
+   */
+  type: 'user_settings'
+  /**
+   *
+   * @type {VDNUserSettingsValue}
+   * @memberof VDNUserSettings
+   */
+  value: VDNUserSettingsValue
+}
