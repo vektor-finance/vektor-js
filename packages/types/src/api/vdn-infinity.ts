08b1d0d59e80e3b3a451566e1f1564112798d0c2
Akram Hussein <akram@vektor.finance>
2023-09-12 16:57:28 -0300

    chore(type): add VDNInfinity

---
+++ packages/types/src/api/vdn-infinity.ts
@@ -0,0 +1,21 @@
+import { VDNBase } from './vdn-base'
+
+/**
+ * VXL Infinity representation
+ * @export
+ * @interface VDNInfinity
+ */
+export interface VDNInfinity extends VDNBase {
+  /**
+   *
+   * @type {string}
+   * @memberof VDNInfinity
+   */
+  type: 'infinity'
+  /**
+   *
+   * @type {any}
+   * @memberof VDNInfinity
+   */
+  value?: undefined
+}
