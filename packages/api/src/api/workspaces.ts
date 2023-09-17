9cc4a3d30aa49b63ebefd266ec7bf5a59faf9404
Akram Hussein <akram@vektor.finance>
2023-09-17 12:14:30 -0300

    feat(api): add delete workspaces and pane

--- packages/api/src/api/workspaces.ts
+++ packages/api/src/api/workspaces.ts
@@ -1,9 +1,25 @@
-import type { FunctionSpec } from '@vektor-finance/types'
-
 import { BaseApi } from '../base'
 import type { ApiResponse } from '../types'
 
 /**
  * An API Client for interacting with Workspaces.
  */
-export class WorkspacesApi extends BaseApi {}
+export class WorkspacesApi extends BaseApi {
+  // Workspaces
+
+  /**
+   * Deletes a Workspace with the specified ID.
+   */
+  public delete(workspaceId: string): Promise<ApiResponse<void>> {
+    return this.httpClient.delete<void>(`/workspaces/${workspaceId}`)
+  }
+
+  // Panes
+
+  /**
+   * Deletes a Pane with the specified ID.
+   */
+  public deletePane(paneId: string): Promise<ApiResponse<void>> {
+    return this.httpClient.delete<void>(`/workspaces/pane/${paneId}`)
+  }
+}
