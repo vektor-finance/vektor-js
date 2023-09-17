1183c0f2a507970ece8f026ba5c7a6c3b63dfc29
Akram Hussein <akram@vektor.finance>
2023-09-17 13:21:48 -0300

    chore(types): update Pane and Workspace

--- packages/types/src/api/pagination.ts
+++ packages/types/src/api/pagination.ts
@@ -4,22 +4,22 @@
  * @interface Pagination
  */
 export interface Pagination {
-    /**
-     * Maximum number of entries to be returned (max = 250)
-     * @type {number}
-     * @memberof Pagination
-     */
-    limit: number
-    /**
-     * Initial position from which to take entries (0-indexed).
-     * @type {number}
-     * @memberof Pagination
-     */
-    offset: number
-    /**
-     * The total number of entries the list holds.
-     * @type {number}
-     * @memberof Pagination
-     */
-    total_count: number | null
-  }
+  /**
+   * Maximum number of entries to be returned (max = 250)
+   * @type {number}
+   * @memberof Pagination
+   */
+  limit: number
+  /**
+   * Initial position from which to take entries (0-indexed).
+   * @type {number}
+   * @memberof Pagination
+   */
+  offset: number
+  /**
+   * The total number of entries the list holds.
+   * @type {number}
+   * @memberof Pagination
+   */
+  total_count: number | null
+}
--- packages/types/src/api/pane.ts
+++ packages/types/src/api/pane.ts
@@ -1,17 +1,40 @@
+import { AnyRuntimeError } from './error'
+import { LocalFunctionCall } from './local-function-call'
 import { Pagination } from './pagination'
+import { VDNOrVDNGeneric } from './vdn'
 
-export type PaneSource = 'static'
-export type PaneState = 'running'
-export interface PaneContent {}
+export interface VDNPaneContent {
+  type: 'vdn'
+  data: VDNOrVDNGeneric
+}
+
+export interface LocalFunctionPaneContent {
+  type: 'local_function'
+  data: LocalFunctionCall
+}
+
+export interface ErrorPaneContent {
+  type: 'error'
+  data: AnyRuntimeError
+}
+
+export type PaneContent = LocalFunctionPaneContent | VDNPaneContent | ErrorPaneContent
+
+export type PaneContentPayloadType = PaneContent['type']
+export type PaneContentPayloadData = PaneContent['data']
+
+export type PaneState = 'running' | 'paused' | 'loading' | 'closed'
+export type PaneSource = 'live' | 'static'
+export type PaneContentType = 'vdn' | 'local_function' | 'error'
 
 export interface Pane {
-    id: string
-    workspace_id: string
-    index: string
-    last_updated_at: string
-    source: PaneSource
-    state: PaneState
-    title: string
-    content: PaneContent
-    pagination: Pagination
+  id: string
+  workspace_id: string
+  index: string
+  last_updated_at: string
+  title: string
+  source: PaneSource
+  content: PaneContent | null
+  state: PaneState
+  pagination: Pagination
 }
