caa6d82e67352b188e115bb8ba2c4aa87e90f377
Akram Hussein <akram@vektor.finance>
2023-09-17 12:09:56 -0300

    feat: stub for WorkspacesApi

---
+++ packages/api/src/api/workspaces.ts
@@ -0,0 +1,9 @@
+import type { FunctionSpec } from '@vektor-finance/types'
+
+import { BaseApi } from '../base'
+import type { ApiResponse } from '../types'
+
+/**
+ * An API Client for interacting with Workspaces.
+ */
+export class WorkspacesApi extends BaseApi {}
