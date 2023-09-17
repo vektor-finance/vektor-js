661bcdafd14550825e7523afd15069a9299b5fd2
Akram Hussein <akram@vektor.finance>
2023-09-17 13:30:38 -0300

    chore: casing

--- packages/api/src/api/labels.ts
+++ packages/api/src/api/labels.ts
@@ -8,28 +8,28 @@ import type { ApiResponse } from '../types'
  */
 export class LabelsApi extends BaseApi {
   /**
-   * Deletes a label with the specified ID.
+   * Deletes a Label with the specified ID.
    */
   public delete(labelId: string): Promise<ApiResponse<void>> {
     return this.httpClient.delete<void>(`/labels/${labelId}`)
   }
 
   /**
-   * Creates a new label.
+   * Creates a new Label.
    */
   public create(payload: LabelCreateRequest): Promise<ApiResponse<Label>> {
     return this.httpClient.post<Label>('/labels', payload)
   }
 
   /**
-   * Retrieves a label with the specified ID.
+   * Retrieves a Label with the specified ID.
    */
   public get(labelId: string): Promise<ApiResponse<Label>> {
     return this.httpClient.get<Label>(`/labels/${labelId}`)
   }
 
   /**
-   * Retrieves all labels.
+   * Retrieves all Labels.
    */
   public list(params?: LabelsListRequestParams): Promise<ApiResponse<Label[]>> {
     return this.httpClient.get<Label[]>('/labels', {
@@ -40,14 +40,14 @@ export class LabelsApi extends BaseApi {
   }
 
   /**
-   * Updates a label with the specified ID.
+   * Updates a Label with the specified ID.
    */
   public update(labelId: string, payload: LabelUpdateRequest): Promise<ApiResponse<Label>> {
     return this.httpClient.put<Label>(`/labels/${labelId}`, payload)
   }
 
   /**
-   * Updates a label partially with the specified ID.
+   * Updates a Label partially with the specified ID.
    */
   public updatePartial(labelId: string, payload: LabelUpdateRequest): Promise<ApiResponse<Label>> {
     return this.httpClient.patch<Label>(`/labels/${labelId}`, payload)
