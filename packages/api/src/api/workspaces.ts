e7b4a91b6d92eb2804b0ccecfde1805044a01a2b
Akram Hussein <akram@vektor.finance>
2023-09-17 13:51:14 -0300

    chore(api/types): remove unnecessary types

--- packages/api/src/api/workspaces.ts
+++ packages/api/src/api/workspaces.ts
@@ -20,14 +20,14 @@ export class WorkspacesApi extends BaseApi {
    * Retrieves a Workspace with the specified ID.
    */
   public get(id: string): Promise<ApiResponse<Workspace>> {
-    return this.httpClient.get<Workspace>(`/workspaces/${id}`)
+    return this.httpClient.get(`/workspaces/${id}`)
   }
 
   /**
    * Retrieves all Workspaces.
    */
   public list(params?: WorkspacesListRequestParams): Promise<ApiResponse<Workspace[]>> {
-    return this.httpClient.get<Workspace[]>('/workspaces', {
+    return this.httpClient.get('/workspaces', {
       params: {
         network_mode: params?.networkMode, // TODO: Remove this?
       },
@@ -38,28 +38,28 @@ export class WorkspacesApi extends BaseApi {
    * Creates a new Workspace.
    */
   public create(payload: WorkspaceCreateRequest): Promise<ApiResponse<Workspace>> {
-    return this.httpClient.post<Workspace>('/workspaces/new', payload)
+    return this.httpClient.post('/workspaces/new', payload)
   }
 
   /**
    * Updates a Workspace.
    */
   public update(id: string, payload: WorkspaceUpdateRequest): Promise<ApiResponse<Workspace>> {
-    return this.httpClient.patch<Workspace>(`/workspaces/${id}`, payload)
+    return this.httpClient.patch(`/workspaces/${id}`, payload)
   }
 
   /**
    * Opens a Workspace.
    */
-  public open(id: string): Promise<ApiResponse<Workspace>> {
-    return this.httpClient.post<Workspace>(`/workspaces/${id}/open`)
+  public open(id: string): Promise<ApiResponse<void>> {
+    return this.httpClient.post(`/workspaces/${id}/open`)
   }
 
   /**
    * Deletes a Workspace with the specified ID.
    */
   public delete(id: string): Promise<ApiResponse<void>> {
-    return this.httpClient.delete<void>(`/workspaces/${id}`)
+    return this.httpClient.delete(`/workspaces/${id}`)
   }
 
   // Panes
@@ -68,20 +68,20 @@ export class WorkspacesApi extends BaseApi {
    * Retrieves a Pane with the specified ID.
    */
   public getPane(id: string): Promise<ApiResponse<Pane>> {
-    return this.httpClient.get<Pane>(`/workspaces/pane/${id}`)
+    return this.httpClient.get(`/workspaces/pane/${id}`)
   }
 
   /**
    * Updates a Pane with the specified ID.
    */
   public updatePane(id: string, payload: PaneUpdateRequest): Promise<ApiResponse<Pane>> {
-    return this.httpClient.patch<Pane>(`/workspaces/pane/${id}`, payload)
+    return this.httpClient.patch(`/workspaces/pane/${id}`, payload)
   }
 
   /**
    * Deletes a Pane with the specified ID.
    */
   public deletePane(id: string): Promise<ApiResponse<void>> {
-    return this.httpClient.delete<void>(`/workspaces/pane/${id}`)
+    return this.httpClient.delete(`/workspaces/pane/${id}`)
   }
 }
