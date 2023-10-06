37430bc7cd2e3f0ca979d2699c9fa8d1b192c0f5
Akram Hussein <akram@vektor.finance>
2023-10-06 16:03:35 -0300

    feat: remove asset, venue and blockchain symbol endpoints since they are unused (#113)

--- packages/api/src/api/assets.ts
+++ packages/api/src/api/assets.ts
@@ -1,4 +1,4 @@
-import type { Asset, AssetsListRequestParams, AssetsSymbolsParams } from '@vektor-finance/types'
+import type { Asset, AssetsListRequestParams } from '@vektor-finance/types'
 
 import { BaseApi } from '../base'
 import type { ApiResponse } from '../types'
@@ -12,17 +12,6 @@ export class AssetsApi extends BaseApi {
    */
   public list(params?: AssetsListRequestParams): Promise<ApiResponse<Asset[]>> {
     return this.httpClient.get('/assets', {
-      params: {
-        network_mode: params?.networkMode,
-      },
-    })
-  }
-
-  /**
-   * Retrieves all Asset symbols.
-   */
-  public symbols(params?: AssetsSymbolsParams): Promise<ApiResponse<string[]>> {
-    return this.httpClient.get('/assets/symbols', {
       params: {
         network_mode: params?.networkMode,
       },
