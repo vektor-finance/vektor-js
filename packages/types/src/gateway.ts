7b492f09bcd7aff58eed3ed211b26d6fc7d13de8
Akram Hussein <akram@vektor.finance>
2023-10-06 14:38:20 -0300

    chore(types): add gateway event to union list

--- packages/types/src/gateway.ts
+++ packages/types/src/gateway.ts
@@ -108,6 +108,7 @@ export type GatewayEvent =
   | StreamUpdatedEvent
   | TaskCompletedEvent
   | TransactionBroadcastedEvent
+  | TransactionUpdatedEvent
   | UserSettingsUpdatedEvent
   | VXLHistoryDeletedEvent
   | VXLHistoryEntryCreatedEvent
