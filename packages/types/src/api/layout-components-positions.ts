e89fd0fcce3afde00eb5d3e4089c98cab334e2e1
Akram Hussein <akram@vektor.finance>
2024-02-16 16:29:40 -0300

    chore: lint fix

--- packages/types/src/api/layout-components-positions.ts
+++ packages/types/src/api/layout-components-positions.ts
@@ -1,83 +1,83 @@
-export type ResizeHandle = "s" | "w" | "e" | "n" | "sw" | "nw" | "se" | "ne";
+export type ResizeHandle = 's' | 'w' | 'e' | 'n' | 'sw' | 'nw' | 'se' | 'ne'
 
 // From 'react-grid-layout'
-interface RGLayout {
-    /**
-     * A string corresponding to the component key.
-     * Uses the index of components instead if not provided.
-     */
-    i: string;
+export interface RGLayout {
+  /**
+   * A string corresponding to the component key.
+   * Uses the index of components instead if not provided.
+   */
+  i: string
 
-    /**
-     * X position in grid units.
-     */
-    x: number;
+  /**
+   * X position in grid units.
+   */
+  x: number
 
-    /**
-     * Y position in grid units.
-     */
-    y: number;
+  /**
+   * Y position in grid units.
+   */
+  y: number
 
-    /**
-     * Width in grid units.
-     */
-    w: number;
+  /**
+   * Width in grid units.
+   */
+  w: number
 
-    /**
-     * Height in grid units.
-     */
-    h: number;
+  /**
+   * Height in grid units.
+   */
+  h: number
 
-    /**
-     * Minimum width in grid units.
-     */
-    minW?: number | undefined;
+  /**
+   * Minimum width in grid units.
+   */
+  minW?: number | undefined
 
-    /**
-     * Maximum width in grid units.
-     */
-    maxW?: number | undefined;
+  /**
+   * Maximum width in grid units.
+   */
+  maxW?: number | undefined
 
-    /**
-     * Minimum height in grid units.
-     */
-    minH?: number | undefined;
+  /**
+   * Minimum height in grid units.
+   */
+  minH?: number | undefined
 
-    /**
-     * Maximum height in grid units.
-     */
-    maxH?: number | undefined;
+  /**
+   * Maximum height in grid units.
+   */
+  maxH?: number | undefined
 
-    /**
-     * set by DragEvents (onDragStart, onDrag, onDragStop) and ResizeEvents (onResizeStart, onResize, onResizeStop)
-     */
-    moved?: boolean | undefined;
+  /**
+   * set by DragEvents (onDragStart, onDrag, onDragStop) and ResizeEvents (onResizeStart, onResize, onResizeStop)
+   */
+  moved?: boolean | undefined
 
-    /**
-     * If true, equal to `isDraggable: false` and `isResizable: false`.
-     */
-    static?: boolean | undefined;
+  /**
+   * If true, equal to `isDraggable: false` and `isResizable: false`.
+   */
+  static?: boolean | undefined
 
-    /**
-     * If false, will not be draggable. Overrides `static`.
-     */
-    isDraggable?: boolean | undefined;
+  /**
+   * If false, will not be draggable. Overrides `static`.
+   */
+  isDraggable?: boolean | undefined
 
-    /**
-     * If false, will not be resizable. Overrides `static`.
-     */
-    isResizable?: boolean | undefined;
+  /**
+   * If false, will not be resizable. Overrides `static`.
+   */
+  isResizable?: boolean | undefined
 
-    /**
-     * By default, a handle is only shown on the bottom-right (southeast) corner.
-     * Note that resizing from the top or left is generally not intuitive.
-     */
-    resizeHandles?: ResizeHandle[] | undefined;
+  /**
+   * By default, a handle is only shown on the bottom-right (southeast) corner.
+   * Note that resizing from the top or left is generally not intuitive.
+   */
+  resizeHandles?: ResizeHandle[] | undefined
 
-    /**
-     * If true and draggable, item will be moved only within grid.
-     */
-    isBounded?: boolean | undefined;
+  /**
+   * If true and draggable, item will be moved only within grid.
+   */
+  isBounded?: boolean | undefined
 }
 
 export type Breakpoint = 'lg' | 'md' | 'sm' | 'xs' | 'xxs'
